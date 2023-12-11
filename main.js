0e4de94378f8c8d7c670ec613f97ef67f8cca309
Voold <barsik.koteevich@mail.ru>
2023-12-11 10:23:25 +0700

    new NEWS bar'

---
+++ main.js
@@ -0,0 +1,53 @@
+let slides = document.querySelectorAll('.slide');
+let slider =[];
+
+console.log(slides);
+
+for (let i = 0; i<slides.length;i++){
+	slider[i] = slides[i].src;
+	slides[i].remove();
+}
+
+console.log(slider);
+
+let step =0;
+let offset =0;
+
+
+
+function slide_show(){
+	let img = document.createElement('img');
+	img.src = slider[step];
+	img.classList.add('slide');
+	img.style.left = offset*0+'px';
+	document.querySelector('#show_panel').appendChild(img);
+	if (step +1 == slider.length){
+		step=0;
+	}
+	else{
+		step++;
+	}
+	offset =1;
+}
+
+slide_show();
+
+function left(){
+	let slides2 = document.querySelectorAll(".slide");
+	let offset2=0;
+	for (let i=0; i<slides2.length;i++){
+		slides2[i].style.left=offset2*0-0+ 'px';
+		offset2++;
+	}
+	setTimeout(function(){
+		slides2[0].remove();
+		slide_show();
+	},1000)
+}
+		
+setInterval(
+  () => left(),
+  5000
+);
+
+document.onclick = left();
